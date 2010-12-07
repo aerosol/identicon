b7a8b7646d0e271618cb06854e55c5eed5856977
adam <adam@adam-VirtualBox.(none)>
2010-12-07 11:56:01 +0100

    Dead simple packaging

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='identicon',
+      description='Python identicon generator',
+      author='Shin Adachi',
+      author_email='shn@glucose.jp',
+      packages=['identicon'],
+     )
